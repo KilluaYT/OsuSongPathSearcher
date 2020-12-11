1df027bd7efc0526ebc9ffc88ca6dc9faae88f4d
KilluaYT <38096703+KilluaYT@users.noreply.github.com>
2020-12-11 11:26:21 +0100

    Add files via upload

---
+++ OsuSongPathSearcher/OsuSongPathSearcher/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace OsuSongPathSearcher
+{
+    public class Class1
+    {
+        
+        public void GetOsuSongPath()
+        {
+
+            String path = "";
+                
+               
+
+                path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                path += (@"\osu!\Songs");
+
+                if (Directory.Exists(path))
+                {
+              
+                OsuSongPath = path;
+                Status = "Auto detected osu! song directory:";
+            }
+                else
+                {
+                    path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    path += (@"\osu!\Songs");
+
+                    if (Directory.Exists(path))
+                    {
+                   
+                    OsuSongPath = path;
+                    Status = "Auto detected osu! song directory:";
+                }
+
+                    else
+                    {
+                        path = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+                        path += (@"\osu!\Songs");
+
+                        if (Directory.Exists(path))
+                        {
+                   
+                        OsuSongPath = path;
+                        Status = "Auto detected osu! song directory:";
+                    }
+                        else
+                        {
+                            path = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+                            path += (@"\osu!\Songs");
+
+                            if (Directory.Exists(path))
+                            {
+                            
+                            OsuSongPath = path;
+                            Status = "Auto detected osu! song directory:";
+                        }
+                            else
+                            {
+ 
+                            OsuSongPath = "";
+                            Status = "Auto detecting osu! song folder failed.";
+                        }
+                        }
+                    }
+                
+            }// just shit of copy+paste to check osu! song folder
+        }
+        public string OsuSongPath;
+        public string Status;
+    }
+}
